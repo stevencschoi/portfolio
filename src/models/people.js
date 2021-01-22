883ad3c1de772db01376c2043b214bd99c420dbe
Steven <stevencschoi@gmail.com>
2021-01-22 00:15:25 -0500

    remade models

--- src/models/people.js
+++ src/models/people.js
@@ -11,8 +11,12 @@ import GspWebp from '../assets/gsp.webp';
 
 export const people = [
   { name: ['Daniel', 'Goldstein'], desc: `Lane 8's music has coloured many influential life experiences in my life to date. He pioneered a movement around eliminating phone use during shows, which serves as a reminder to try and be present in each moment.`, pic: Lane8, webp: Lane8Webp },
-  { name: ['Dave', 'Chappelle'], desc: `They say comedians are the modern day philosopher. I am inspired by Dave Chappelle's wit and humour whilst delivering perceptive social commentary. His infamous departure from TV and more specifically, a $50 million contract, is an example of a man choosing morality over material, which seems so rare nowadays.`, pic: Chappelle, webp: ChappelleWebp },
+  
   { name: ['Kobe', 'Bryant'], desc: `Tireless work ethic. Relentless competitor. The Mamba Mentality - to strive for greatness at any cost. Kobe's passing in 2020 was a stark reminder to live life with purpose. Shortly after, I enrolled in Bootcamp and hope to use his example as motivation to push past my limits. After all, what kind of person would stare into the sun for days on end because of one missed shot? Kobe. Bryant. Rest in peace.`, pic: Kobe, webp: KobeWebp },
-  { name: ['Stephen', 'Curry'], desc: `Not the biggest, tallest or fastest baller around, but Steph plays an exciting and most importantly, winning brand of basketball. He's the embodiment of not judging a book by its cover. By all accounts, he is an incredible teammate and leader - traits I try to practice and build upon.`, pic: Steph, webp: StephWebp },
+
+  { name: ['Dave', 'Chappelle'], desc: `They say comedians are the modern day philosopher. I am inspired by Dave Chappelle's wit and humour whilst delivering perceptive social commentary. His infamous departure from TV and more specifically, a $50 million contract, is an example of a man choosing morality over material, which seems so rare nowadays.`, pic: Chappelle, webp: ChappelleWebp },
+  
+  { name: ['Stephen', 'Curry'], desc: `Not the biggest, tallest or fastest baller around, but Steph plays an exciting and most importantly, winning brand of basketball. He's the embodiment of not judging a book by its cover. At the same time, it's clear Steph has loads of fun, chucking from anywhere and dancing on anyone. It's a pleasure to watch and a reminder to lead each day with joy.`, pic: Steph, webp: StephWebp },
+
   { name: ['Georges', 'St.', 'Pierre'], desc: `Georges St. Pierre is a refreshingly kind and self-aware individual in a sea of bravado. He routinely discusses his fears and praises his opponents as they insult his ability and character. He usually proceeds to kick their collective butts with ease, which makes his sportsmanship even more remarkable. I just think GSP is a good person, and exemplifies the type of man I aspire to be.`, pic: Gsp, webp: GspWebp },
 ]
